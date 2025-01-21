f10b5051f37744f58bf3a7e34d7675b4c5440e79
oscar066 <oscarkaruga1@gmail.com>
2025-01-21 13:02:59 +0300

    :construction: Add the svg domain then move from domains to remotepatterns

--- frontend-next/next.config.mjs
+++ frontend-next/next.config.mjs
@@ -3,7 +3,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["127.0.0.1"], // Add your local domain here77
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        pathname: '/6.x/initials/svg',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+      }
+    ],
+    dangerouslyAllowSVG: true,
   },
 };
 
